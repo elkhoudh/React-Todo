43b856ee47463a81af645e19e138b24bea6d94b4
Hamza Elkhoudiri <elkhoudh@gmail.com>
2019-01-10 14:15:04 -0500

    Added a style

--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,17 +1,30 @@
 import React from "react";
 import "./Todo.css";
+import { withStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+
+const styles = theme => ({
+  root: {
+    ...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+    paddingBottom: theme.spacing.unit * 2,
+    margin: 20
+  }
+});
 
 const TodoList = props => {
+  const { classes } = props;
+
   return (
-    <div>
+    <Paper className={classes.root} elevation={1}>
       <p
         className={props.todo.completed ? "line list" : "list"}
         onClick={e => props.handleCompleted(e, props.todo.id)}
       >
         {props.todo.task}
       </p>
-    </div>
+    </Paper>
   );
 };
 
-export default TodoList;
+export default withStyles(styles)(TodoList);
